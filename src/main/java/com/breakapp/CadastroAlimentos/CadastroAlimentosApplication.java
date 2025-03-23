e110b1bbcb7fad100e6aea88d7b47caecd8d8c63
vinic <viniciusscc24@gmail.com>
2025-03-23 14:36:28 -0300

    first commit

---
+++ src/main/java/com/breakapp/CadastroAlimentos/CadastroAlimentosApplication.java
@@ -0,0 +1,13 @@
+package com.breakapp.CadastroAlimentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroAlimentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroAlimentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/breakapp/CadastroAlimentos/CadastroAlimentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.breakapp.CadastroAlimentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroAlimentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
